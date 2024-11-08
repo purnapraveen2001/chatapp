df0cb58fd79ea0d2bebaf83c695e179e96920e6c
DivyaSusmitha2000 <divyasusmitha.nidamanuri@gwu.edu>
2024-11-08 06:36:59 -0500

    intial commit

---
+++ chat_app_backend/backend/accounts/views.py
@@ -0,0 +1,33 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import UserSerializer
+from .serializers import LoginSerializer
+from rest_framework import status
+from accounts.tokenauthentication import JWTAuthentication
+from django.contrib.auth import authenticate
+
+
+
+@api_view(['POST'])
+def register_user(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
+
+@api_view(['POST'])
+def login(request):
+    serializer = LoginSerializer(data=request.data)
+    if serializer.is_valid():
+        token = JWTAuthentication.generate_token(payload=serializer.data)
+        return Response({
+            "message": "Login Successfull",
+            'token': token,
+            'user': serializer.data
+        }, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
